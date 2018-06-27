f1acbd24f8b75267a9ce91e397b8f876d9840945
Vincent Lansardiere <vincent.lansardiere@gmail.com>
2018-06-27 18:23:30 +0200

    debug + ajout assoc-member

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -6,6 +6,7 @@ import { DixEventModule } from './event/event.module';
 import { DixCategoryModule } from './category/category.module';
 import { DixUserEventParticipationModule } from './user-event-participation/user-event-participation.module';
 import { DixAssociationUserRoleModule } from './association-user-role/association-user-role.module';
+import { DixAssoc_membersModule } from './assoc-members/assoc-members.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
@@ -16,6 +17,7 @@ import { DixAssociationUserRoleModule } from './association-user-role/associatio
         DixCategoryModule,
         DixUserEventParticipationModule,
         DixAssociationUserRoleModule,
+        DixAssoc_membersModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
